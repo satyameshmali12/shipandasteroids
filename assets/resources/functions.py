b73ffea6ce3f2e7e2c59fb0d7fac4ea697751406
Satyamesh Mali <106645165+satyameshmali12@users.noreply.github.com>
2022-08-07 23:55:46 +0530

    Added new files

---
+++ assets/resources/functions.py
@@ -0,0 +1,30 @@
+import pygame
+import os
+
+
+def loadimage(name):
+    img = pygame.image.load(name)
+    return img
+
+def scaleimage(name,x,y):
+    scaledimg = pygame.transform.scale(name,(x,y))
+    return scaledimg
+
+def displayimage(display,name,x,y):
+    display.blit(name,(x,y))
+
+def displaytext(display,text,x,y,size,color,bold,italic):
+    font = pygame.font.SysFont(None,size,bold,italic)
+    text = font.render(text,True,color)
+    display.blit(text,(x,y))
+
+def listallthefiles(name):
+    dir = os.listdir(name)
+    images = []
+    for i in range(len(dir)):
+        images.append(pygame.image.load(f"{name}/{dir[i]}"))        
+    return images
+
+def playmusic(name):
+    pygame.mixer.music.load(name)
+    pygame.mixer.music.play()
